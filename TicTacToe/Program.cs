19f5179fb18791cdc0b94bb2f237700bb8059e30
alex-pope <alexanderspope@bex.net>
2013-03-01 19:15:08 -0500

    Added Program.cs file.

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            // Create and show a new SplashScreen.
+            SplashScreen aSplashScreen = new SplashScreen();
+            aSplashScreen.ShowDialog();
+
+            Application.Run(new TicTacToe());
+        }
+    }
+}
